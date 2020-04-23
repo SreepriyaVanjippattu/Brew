5c6bd831ef4be6432c09aee693c7ef35cb7593d7
Sreepriyavp <sreepriyav@nirasystems.com>
2020-04-23 20:18:52 +0530

    Side menu

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -31,6 +31,11 @@ export const MENU_ITEMS_SUPER_USER: NbMenuItem[] = [
     icon: 'role-ico',
     link: '/app/role-privileges',
   },
+  {
+    title: "Yeast Manager",
+    icon: "yeast-ico",
+    link: "/app/yeaststrains",
+  },
 ];
 
 export const MENU_ITEMS_ORGANIZATION: NbMenuItem[] = [
