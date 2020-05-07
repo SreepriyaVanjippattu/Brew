a5776a7944d85c6be4abf1edeb9fdb29ba4cbca7
Sreepriyavp <sreepriyav@nirasystems.com>
2020-05-07 15:47:37 +0530

    Removed Super admin details from brewersInsight

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -14,66 +14,7 @@ const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
-     /** wt-admin routes */
-     {
-      path: 'clients',
-      loadChildren: () => import('../pages/wt-admin/clients/clients.module')
-        .then(m => m.ClientsModule),
-
-      canActivateChild: [RoleGaurdService, AuthGaurdService],
-      canActivate: [RoleGaurdService, AuthGaurdService],
-      data: {
-        expectedRole: [Constants.Superadmin],
-        expectedPermission: ['Delete Organization'],
-      },
-
-    },
-    {
-      path: 'reports-admin',
-      loadChildren: () => import('../pages/wt-admin/reports-admin/reports-admin.module')
-        .then(m => m.ReportsAdminModule),
-      canActivate: [RoleGaurdService, AuthGaurdService],
-      data: {
-        expectedRole: [Constants.Superadmin],
-      },
-    },
-    {
-      path: 'user-directory-admin',
-      loadChildren: () => import('../pages/wt-admin/user-directory/user-directory.module')
-        .then(m => m.UserDirectoryModule),
-      canActivate: [RoleGaurdService, AuthGaurdService],
-      data: {
-        expectedRole: [Constants.Superadmin],
-      },
-    },
-    {
-      path: 'settings',
-      loadChildren: () => import('../pages/wt-admin/settings/settings.module')
-        .then(m => m.SettingsModule),
-      canActivate: [RoleGaurdService, AuthGaurdService],
-      data: {
-        expectedRole: [Constants.Superadmin],
-      },
-    },
-    {
-      path: 'profile-wt-admin',
-      loadChildren: () => import('../pages/wt-admin/profile-super-admin/profile-super-admin.module')
-        .then(m => m.ProfileSuperAdminModule),
-      canActivate: [RoleGaurdService, AuthGaurdService],
-      data: {
-        expectedRole: [Constants.Superadmin],
-      },
-    },
-    {
-      path: 'role-privileges',
-      loadChildren: () => import('../pages/wt-admin/role-privileges-super-admin/role-privileges-super-admin.module')
-        .then(m => m.RolePrivilegesSuperAdminModule),
-      canActivate: [RoleGaurdService, AuthGaurdService],
-      data: {
-        expectedRole: [Constants.Superadmin],
-      },
-    },
-    /** wt-admin routes */
+     
     {
       path: 'dashboard',
       loadChildren: () => import('../pages/organization-users/dashboard/dashboard.module')
